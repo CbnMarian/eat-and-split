5879d22525a01e25dc7a9edf84e9cc4123f6880e
CbnMarian <m.marian03@yahoo.com>
2024-01-16 13:04:15 +0200

    split

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import { useState } from "react";
 const initialFriends = [
   {
     id: 118836,
-    name: "Clark",
+    name: "Marian",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
   },
   {
     id: 933372,
-    name: "Sarah",
+    name: "Andrei",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
     id: 499476,
-    name: "Anthony",
+    name: "Alex",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
